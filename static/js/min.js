0594c8819b9bbb99effc8bd6c72dda3a727c8f15
PRATIK BHARAT GAONKAR <107390913+PBG45@users.noreply.github.com>
2023-12-08 21:11:11 +0530

    Update min.js

--- static/js/min.js
+++ static/js/min.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
       $("#plus6").animate({ rotate: '0deg' });
 
       if ($(window).width() < 600) {
-        $(".sub-content3").css("height", "150vh");
+        $(".sub-content3").css("height", "180vh");
       } else {
         $(".sub-content3").css("height", "150vh");
       }
@@ -73,7 +73,7 @@ $(document).ready(function () {
         $("#plus2").animate({ rotate: "0deg" }, "slow");
 
         if ($(window).width() < 600) {
-          $(".sub-content3").css("height", "150vh");
+          $(".sub-content3").css("height", "180vh");
         }
 
         if ($(window).width() > 600) {
